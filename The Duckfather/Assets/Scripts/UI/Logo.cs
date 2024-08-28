1c3409dd5d4d438cd65701984616e08159cabf45
Daniel Arango <77700657+danielarango32@users.noreply.github.com>
2024-08-27 23:46:08 -0500

    cambios pequeños

--- The Duckfather/Assets/Scripts/UI/Logo.cs
+++ The Duckfather/Assets/Scripts/UI/Logo.cs
@@ -11,6 +11,7 @@ public class Logo : MonoBehaviour
 {
     public GameObject logoDF;
     public GameObject Sutdio;
+    public GameObject skipButton;
     [SerializeField] private VideoPlayer videoPlayer;
 
 
@@ -27,11 +28,17 @@ public class Logo : MonoBehaviour
         // For example, you can use the following code to change the scene  
         SceneManager.LoadScene("Online 2");
     }
+
+    public void Skip()
+    {
+        SceneManager.LoadScene("Online 2");
+    }
     IEnumerator LogoTime()
     {
         yield return new WaitForSeconds(2);
         logoDF.SetActive(false);
         Sutdio.SetActive(true);
+        skipButton.SetActive(true);
         yield break;
     }
 
